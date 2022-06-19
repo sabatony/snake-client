e18a86c570f70b6bc8b12b325ff309be5e6f6fe8
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-06-18 22:53:40 -0700

    refactor code by adding module

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+
+const connect = function () {
+  
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("end", () => {
+    console.log('thanks for playing');
+  })
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,21 +1,8 @@
-const net = require("net");
-
+ 
 // establishes a connection with the game server
-const connect = function () {
-  
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("end", () => {
-    console.log('thanks for playing');
-  })
+const { connect } = require('./client');
 
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
