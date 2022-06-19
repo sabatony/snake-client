3359912a63cd594fb60ed576432048402345280e
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-06-18 23:15:23 -0700

    add greeting on connection

--- client.js
+++ client.js
@@ -11,9 +11,15 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: SAB");
+
+  });
+
   conn.on("end", () => {
     console.log('thanks for playing');
-  })
+  });
 
   return conn;
 };
