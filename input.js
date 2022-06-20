bd013698157e384e37bffcba5532b5874ad8e7be
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-06-20 12:46:37 -0700

    add messages

--- input.js
+++ input.js
@@ -5,16 +5,22 @@ const handleUserInput = function (key) {
     process.exit();
   }
   if (key === 'w') {
-    connection.write('Move: up')
+    connection.write('Move: up');
   }
   if (key === 'a') {
-    connection.write('Move: left')
+    connection.write('Move: left');
   }
   if (key === 's') {
-    connection.write('Move: down')
+    connection.write('Move: down');
   }
   if (key === 'd') {
-    connection.write('Move: right')
+    connection.write('Move: right');
+  }
+  if (key === '1') {
+    connection.write('Say: yum')
+  }
+  if (key === '2') {
+    connection.write('Say: =)')
   }
 
 };
